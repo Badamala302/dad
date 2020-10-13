86b2e1c1d22fc244397af361a5e363dad345ccaf
Badamala302 <72606242+Badamala302@users.noreply.github.com>
2020-10-13 15:38:55 +0500

    Update ahamad.py

--- ahamad.py
+++ ahamad.py
@@ -12,37 +12,30 @@
 #Reserved2020
 
 # A workflow run is made up of one or more jobs that can run sequentially or in parallel
-jobs: "cloning"
+
   # This workflow contains a single job called "build"
-  build:"child"
+ 
     # The type of runner that the job will run on
-    runs-on: ubuntu-latest
+
 
     # Steps represent a sequence of tasks that will be executed as part of the job
-    steps:
+
       # Checks-out your repository under $GITHUB_WORKSPACE, so your job can access it
-      - uses: actions/checkout@v2
+      
 
       # Runs a single command using the runners shell
-      - name: Run a one-line script
-        run: echo Hello, world!
+      - 
 
       # Runs a set of commands using the runners shell
-      - name: Run a multi-line script
-        run: |
-          echo Add other actions to build,
-          echo test, and deploy your project.
-[10/12, 1:16 AM] BILALO DADX: !/usr//python2
+      - 
 #coding=utf-8
 #The Credit For This Code Goes To MR-SH4DOW
 #If You Wanna Take Credits For This Code, Please Look Yourself Again...
 #Reserved2020
 
 
-import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,requests,mechanize
-from multiprocessing.pool import ThreadPool
-from requests.exceptions import ConnectionError
-from mechanize import Browser
+
+from 
 
 
 reload(sys)
